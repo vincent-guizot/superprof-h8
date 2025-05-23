923d781a14b74168af958bfacaf5d39fbf33c827
Vincent Guizot <30890039+vincent-guizot@users.noreply.github.com>
2025-05-23 10:02:43 +0700

    Update index.js

--- fase-1/Diy Apps/routes/index.js
+++ fase-1/Diy Apps/routes/index.js
@@ -4,12 +4,9 @@ const router = express.Router();
 const authorRouters = require("./authors");
 const postRouters = require("./posts");
 
-// route
-// app.METHOD(PATH/ENDPOINT, HANDLER/FUNCTION)
-// ? Static route
 router.get("/", Controller.home);
 
-// router.use("/authors", authorRouters);
-// router.use("/posts", postRouters);
+router.use("/authors", authorRouters);
+router.use("/posts", postRouters);
 
 module.exports = router;
