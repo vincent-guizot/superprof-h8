90b886d1affd31ca97c4d74675204ce4f43e14a8
Vincent Guizot <30890039+vincent-guizot@users.noreply.github.com>
2025-05-23 11:11:56 +0700

    Update index.js

--- fase-1/Diy Apps/controllers/index.js
+++ fase-1/Diy Apps/controllers/index.js
@@ -2,6 +2,23 @@ class Controller {
   static home(req, res) {
     res.render("home.ejs");
   }
+
+  static async getAuthors(req,res){
+    try {
+      //
+    } catch(err){
+      res.json(err
+    }
+  }
+
+  static async getPosts(req,res){
+    try {
+      //
+    } catch(err){
+      res.json(err
+    }
+  }
+  
 }
 
 module.exports = Controller;
