96632974356f6ce6f10aea0cb76b4bff240c1b69
Vincent Guizot <30890039+vincent-guizot@users.noreply.github.com>
2025-05-23 09:56:34 +0700

    Update class.js

---
+++ fase-1/Diy Apps/models/class.js
@@ -0,0 +1,39 @@
+
+
+class Author {
+  constructor(id, fullName, gender){
+    this.id = id;
+    this.fullName = fullName;
+    this.gender = gender;
+  }
+}
+
+class AuthorDetail extends Author {
+  constructor(id, fullName, gender, totalPost, totalVote, averageTime){
+    super(id, fullName, gender);
+    this.totalPost = totalPost;
+    this.totalVote = totalVote;
+    this.averageTime = averageTime;
+  }
+}
+
+class Post {
+  constructor(id, title, difficulty, totalVote){
+    this.id = id;
+    this.title = title;
+    this.difficulty = difficulty;
+    this.totalVote = totalVote;
+  }
+}
+
+class PostDetail extends Post {
+  constructor(id, title, difficulty, totalVote, estimatedTime, description, imageUrl, createdDate, AuthorId, authorName){
+    super(id, title, difficulty, totalVote)
+    this.estimatedTime = estimatedTimel;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.createdDate = createdDate;
+    this.AuthorId = AuthorId;
+    this.authorName = authorName;
+  }
+}
