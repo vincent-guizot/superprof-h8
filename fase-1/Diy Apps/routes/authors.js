5865731a861e1bd54801ff5df83768676bd8cac0
vincent-guizot <vincentguizot128@gmail.com>
2025-05-24 09:42:31 +0700

    routes

--- fase-1/Diy Apps/routes/authors.js
+++ fase-1/Diy Apps/routes/authors.js
@@ -2,4 +2,7 @@ const express = require("express");
 const Controller = require("../controllers");
 const authorRoute = express.Router();
 
+authorRoute.get("/", Controller.getAuthors);
+authorRoute.get("/detail", Controller.getAuthorById);
+
 module.exports = authorRoute;
